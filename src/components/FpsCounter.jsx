6b632c44bf7f9fc778045e3a6ecb33d619a231a0
Daniel Gustaw <gustaw.daniel@gmail.com>
2024-10-27 01:14:45 +0400

    low fps counter fixed

--- src/components/FpsCounter.jsx
+++ src/components/FpsCounter.jsx
@@ -2,10 +2,11 @@ import { useFPS } from "../FpsContext.jsx";
 
 // Usage in a component
 export default function FpsCounter() {
-  const { fps, insufficientComputingPower } = useFPS();
+  const { fps, insufficientComputingPower, lowFpsCount } = useFPS();
 
   return (
     <li
+      title={`Low FPS counted ${lowFpsCount} times`}
       className={insufficientComputingPower ? "text-red-500" : "text-green-500"}
     >
       FPS: {fps}
